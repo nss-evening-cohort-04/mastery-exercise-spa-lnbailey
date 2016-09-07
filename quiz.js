d8dd1091f7e09c360a4b3edb6aefa7c884b69a5c
Ellen <lnduncklee@gmail.com>
2016-09-06 19:59:14 -0500

    add rest of initial files

--- quiz.js
+++ quiz.js
@@ -1,1 +1,12 @@
 document.write("Cause I'm ready for the next challenge");
+
+function populatePage (inventory) {
+  // Loop over the inventory and populate the page
+
+  // Now that the DOM is loaded, establish all the event listeners needed
+  CarLot.activateEvents();
+}
+
+// Load the inventory and send a callback function to be
+// invoked after the process is complete
+CarLot.loadInventory();
