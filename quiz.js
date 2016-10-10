9600aec714ed1d0aeb345da28b1eaee7b25880a7
Ellen <lnduncklee@gmail.com>
2016-10-10 18:00:46 -0500

    getting started

--- quiz.js
+++ quiz.js
@@ -1,1 +1,22 @@
-document.write("Cause I'm ready for the next challenge");
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function () {
+	if (xhr.status === 200) {
+		responseObject = JSON.parse(xhr.responseText);
+
+	var newContent = '';
+	for (var i = 0; i < responseObject.cars.length; i++) {
+		newContent += '<div class="object">';
+		newContent += responseObject.cars[i].make + '<br>';
+		newContent += responseObject.cars[i].model + '<br>';
+		newContent += responseObject.cars[i].year + '<br>';
+		newContent += responseObject.cars[i].price + '<br>';
+		newContent += responseObject.cars[i].description + '<br>';
+		newContent += '</div>';
+		}
+	document.getElementById('car-inventory').innerHTML = newContent;	
+	}
+};
+
+xhr.open('GET', 'inventory.json', true);
+xhr.send(null);
